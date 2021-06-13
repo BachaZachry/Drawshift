79d35414ae0d63f16276e732f9ad70b0929ee71f
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2021-06-13 11:55:57 +0200

    Corrected the return of the consumer message fnc

--- app/app/chat/consumers.py
+++ app/app/chat/consumers.py
@@ -21,29 +21,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-    
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        name = text_data_json['name']
-
+        # message = text_data_json['message']
+        # message = json.loads(text_data)
         # Send message to room group
+       
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'name': name
+                'message': text_data_json,
             }
         )
     
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-        name = event['name']
-
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
-            'name': name
+            
         }))
+        # await self.send(text_data=message)
