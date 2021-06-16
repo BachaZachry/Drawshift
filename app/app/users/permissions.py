91d939c06324145f72c714ec926eafae10cfaedd
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2021-06-16 19:55:24 +0200

    Added permissions

---
+++ app/app/users/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+
+
+class canCreateTeam(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (request.user.team == None)
+
+
+class canInvite(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.can_invite
+
+
+class hasTeam(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (request.user.team != None)
