1110f3095ee05bbe03203bc61927bb8fe1f82b48
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2022-02-01 19:07:28 +0100

    added logout

--- app/app/users/api/urls.py
+++ app/app/users/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include, re_path
 from .views import CreateTeam, GithubLogin, InviteAPIView, RegisterAPI, LoginAPI, LoadUser,\
     GoogleLogin, RespondToAnInvitation
+from knox.views import LogoutView
 
 urlpatterns = [
     path('login/', LoginAPI.as_view()),
     path('loaduser/', LoadUser.as_view()),
     path('register/', RegisterAPI.as_view()),
+    path('logout/', LogoutView.as_view()),
     re_path(r'^rest-auth/google/$', GoogleLogin.as_view(), name='google_login'),
     path('invite/', InviteAPIView.as_view()),
     re_path(r'^invite/(?P<pk>\d+)/$', RespondToAnInvitation.as_view()),
