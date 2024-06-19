94847b5cb4f8891ebe1d1047d86b10838509ab77
BachaZachry <b.ilyeszakaria@gmail.com>
2024-06-19 23:40:10 +0100

    update login return

--- users/api/views.py
+++ users/api/views.py
@@ -50,11 +50,13 @@ class LoginAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
-        token = AuthToken.objects.create(user)
+
         return Response(
             {
-                "token": token[1],
-                "username": user.username,
+                "user": UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+                "token": AuthToken.objects.create(user)[1],
             }
         )
 
